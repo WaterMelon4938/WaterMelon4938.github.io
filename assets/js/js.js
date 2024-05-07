5dc24cec13581636c6a66624a5abaf723de169aa
WaterMelon4938 <shaughndwilliamson@gmail.com>
2024-05-07 11:02:24 -0400

    updated
    This is a school project.

---
+++ assets/js/js.js
@@ -0,0 +1,26 @@
+/* clock */
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+  /* submit popup */
+  function popupSubmit() {
+    alert("Submitted");
+  }
+
+  /* cancel popup */
+  function popupCancel() {
+    alert("Canceled");
+  }
